d12bf89c4effda02009419c35c44bec87a97758b
souza_filipe <souza_filipe@sicredi.com.br>
2025-05-09 15:35:26 -0300

    Initial Commit

---
+++ GitHubRepositories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubRepositories
+//
+//  Created by Filipe de Souza on 09/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubRepositoriesTests/GitHubRepositoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesTests.swift
+//  GitHubRepositoriesTests
+//
+//  Created by Filipe de Souza on 09/05/25.
+//
+
+import Testing
+@testable import GitHubRepositories
+
+struct GitHubRepositoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
