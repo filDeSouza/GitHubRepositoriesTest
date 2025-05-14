0861bd8e0b9bdf8d9cfc12d5e1437d0f79f53662
filDeSouza <fil.desouza@gmail.com>
2025-05-14 17:32:57 -0300

    Remove keys values

--- GitHubRepositories/Utils/Keys.swift
+++ GitHubRepositories/Utils/Keys.swift
@@ -8,6 +8,5 @@
 import Foundation
 
 class Keys {
-    // A Api do git possui uma restrição do número de requisições realizadas, por esse motivo essa chave esta incluída. Para o push, essa chave esta sendo comentada
-//    let gitToken = "token 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"
+    // A Api do git possui uma restrição do número de requisições realizadas, por esse motivo essa chave esta incluída. Para o push, essa chave esta sendo retirada
 }
